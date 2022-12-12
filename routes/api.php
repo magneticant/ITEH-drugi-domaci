8bb0805cdd9c6d39feaf8f8fd94dff3d31eec402
Milan <milance.za019@gmail.com>
2022-12-12 14:33:54 +0100

    Dodao sam metode za registraciju i login korisnika u AuthController-u.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\DoctorAppointmentController;
 use App\Http\Controllers\DoctorController;
@@ -32,12 +33,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Route::get('/users', [UserController::class, 'index']);
 //Route::get('/users/{id}', [UserController::class, 'show']);
-Route::resource('appointments', AppointmentController::class);
+// Route::resource('appointments', AppointmentController::class);
 Route::resource('doctors', DoctorController::class);
 Route::resource('users', UserController::class);
 
 //Ugnjezdeni resursi
 
 Route::get('/users/{id}/appointments', [UserAppointmentController::class, 'index'])->name('users.appointments.index');
-Route::get('/users/{id}/doctors', [UserDoctorController::class, 'index'])->name('users.doctors.index');
-Route::get('/doctors/{id}/appointments', [DoctorAppointmentController::class, 'index'])->name('doctors.appointments.index');
+Route::resource('users.doctors', UserDoctorController::class);
+// Route::get('/doctors/{id}/appointments', [DoctorAppointmentController::class, 'index'])->name('doctors.appointments.index');
+Route::resource('doctors.appointments', DoctorAppointmentController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
